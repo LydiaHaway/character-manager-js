b4ffbda0fe2928d74528f348d9f47db1749c1601
Lydia <lydia.haway@gmail.com>
2022-06-27 16:37:49 +0200

    ex

--- src/index.js
+++ src/index.js
@@ -2,4 +2,45 @@ const axios = require("axios");
 
 import "./scss/style.scss";
 
+//_____________________________________________________________________
+
 //______________________________________________________________________
+
+const URL = `https://character-database.becode.xyz/characters`;
+
+axios
+  .get(URL)
+  .then((response) => {
+    const main = document.querySelector("main");
+
+    const container = document.createElement("section");
+    container.setAttribute("class", "container_characters");
+    main.appendChild(container);
+
+    let displayCharacters = (e) => {
+      const containerCharacter = document.createElement("div");
+      containerCharacter.setAttribute("class", "container_character");
+      container.appendChild(containerCharacter);
+
+      const image = document.createElement("img");
+      image.src = "data:image/png;base64, " + response.data[e].image;
+      containerCharacter.appendChild(image);
+
+      const name = document.createElement("h2");
+      name.textContent = response.data[e].name;
+      containerCharacter.appendChild(name);
+
+      const shortDescriprt = document.createElement("p");
+      shortDescriprt.textContent = response.data[e].shortDescription;
+      containerCharacter.appendChild(shortDescriprt);
+    };
+
+    displayCharacters(0);
+    displayCharacters(1);
+    displayCharacters(2);
+    displayCharacters(3);
+    displayCharacters(4);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
