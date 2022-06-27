6deb5c54975133e1446acefec523c85cca014390
Lydia <lydia.haway@gmail.com>
2022-06-27 14:48:01 +0200

    ex

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  mode: "development",
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
+
+// npm run dev
