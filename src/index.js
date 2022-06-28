a2e1ea462892207d94132a681c9d5b3ed3883020
Lydia <lydia.haway@gmail.com>
2022-06-28 12:32:45 +0200

    ex

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ const main = document.querySelector("main");
 
 const container = document.querySelector(".container_characters");
 
+let inputSearch = document.querySelector("#input_search");
+
+const buttonSearch = document.querySelector(".search");
+
 //______________________________________________________________________
 
 const displayCharacters = async () => {
@@ -34,14 +38,11 @@ const displayCharacters = async () => {
       containerCharacter.appendChild(shortDescriprt);
 
       const linkProfile = document.createElement("a");
+      linkProfile.setAttribute("class", "link_profile");
       linkProfile.textContent = "profile";
       containerCharacter.appendChild(linkProfile);
-      linkProfile.href = "exemple.com";
+      linkProfile.href = "./profile.html";
     };
-
-    for (let i = 0; i > 99; i++) {
-      displayCharacter(i);
-    }
 
     displayCharacter(0);
     displayCharacter(1);
@@ -50,9 +51,71 @@ const displayCharacters = async () => {
     displayCharacter(4);
     displayCharacter(5);
     displayCharacter(6);
+    displayCharacter(7);
+    displayCharacter(8);
+    displayCharacter(9);
   } catch (err) {
     console.error(err);
   }
 };
 
+//_____________________________________________________________________________
+
 displayCharacters();
+
+//_____________________________________________________________________________
+
+const displayCharactersSearch = async () => {
+  try {
+    const response = await axios.get(
+      `https://character-database.becode.xyz/characters?name=${inputSearch.value}`
+    );
+
+    const containerCharacterSearch = document.createElement("div");
+    containerCharacterSearch.setAttribute("class", "container_Search");
+    main.appendChild(containerCharacterSearch);
+
+    const image = document.createElement("img");
+    image.src = "data:image/png;base64, " + response.data[0].image;
+    containerCharacterSearch.appendChild(image);
+
+    const name = document.createElement("h2");
+    name.textContent = response.data[0].name;
+    containerCharacterSearch.appendChild(name);
+
+    const shortDescriprt = document.createElement("p");
+    shortDescriprt.textContent = response.data[0].shortDescription;
+    containerCharacterSearch.appendChild(shortDescriprt);
+
+    const linkProfile = document.createElement("a");
+    linkProfile.textContent = "profile";
+    linkProfile.setAttribute("class", "link_profile");
+    containerCharacterSearch.appendChild(linkProfile);
+    linkProfile.href = "exemple.com";
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+//____________________________________________________________________________
+
+const containerCharacter = document.querySelector(".container_character");
+const ButtonReturn = document.querySelector(".return");
+
+buttonSearch.addEventListener("click", () => {
+  container.remove(containerCharacter);
+  displayCharactersSearch();
+});
+
+document.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    container.remove(containerCharacter);
+    displayCharactersSearch();
+  }
+});
+
+ButtonReturn.addEventListener("click", () => {
+  location.reload();
+});
+
+//___________________________________________________________________________
